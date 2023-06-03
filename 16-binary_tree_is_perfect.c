17628fae691954545b2feae5464ba098c619ea51
Gibexz <andrewetedu@gmail.com>
2023-06-03 22:11:04 +0100

    updates on Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,12 +1,41 @@
 #include "binary_trees.h"
-#include "binary_trees.h"
+/**
+ * power - calculates the power exponent of the base
+ * @base: base of the power
+ * @exponent: exponent of the power
+ *
+ * Return: The power
+ */
+int power(int base, int exponent)
+{
+	int result = 1;
+
+	for (; exponent > 0; exponent--)
+	{
+	result = result * base;
+	}
+	return (result);
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: binary tree
+ *
+ * Return: size = number of nodes
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
 /**
  * binary_tree_height - function that returns the height of a binary tree
  * @tree: pointer to the binary tree
  *
  * Return: height of the binary tree
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l = 0, r = 0;
@@ -35,19 +64,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int height, size, perfect_size;
+
 	if (tree)
 	{
-		if (!tree->left && !tree->right)
+		height = binary_tree_height(tree);
+		size = binary_tree_size(tree);
+		perfect_size = (power(2, height + 1)) - 1;
+		if (size == perfect_size)
 			return (1);
-		if (!tree->left || !tree->right)
-			return (0);
-		if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
-			return (0);
 		else
-			return (1);
-
-		return (1 * binary_tree_is_perfect(tree->right)
-				* binary_tree_is_perfect(tree->left));
+			return (0);
 	}
 	return (0);
 }
